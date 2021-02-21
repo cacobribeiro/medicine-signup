34927972a991c011125c237c8bac8244be9d9c68
CAIO RIBEIRO <cacobribeiro@gmail.com>
2021-02-21 19:15:57 -0300

    config initial of sequelize

---
+++ back-end/models/index.ts
@@ -0,0 +1,20 @@
+import * as sequelize from 'sequelize'
+import { Register } from './register'
+
+export const dbConfig = new sequelize.Sequelize(
+  {
+    username: process.env.MYSQL_USER || 'root',
+    database: process.env.DB_NAME || 'gcb-data',
+    port: Number(process.env.DB_PORT) || 3306,
+    host: process.env.HOSTNAME || 'localhost',
+    dialect: 'mysql',
+    pool: {
+      min: 0,
+      max: 5,
+      acquire: 30000,
+      idle: 10000
+    }
+  }
+)
+
+export const User = Register(dbConfig)
